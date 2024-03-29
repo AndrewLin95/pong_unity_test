84900861c78e7af30562f9d23a7f1c85f5f6afc9
AndrewLin95 <andrew.lin88@hotmail.com>
2024-03-29 17:45:41 -0400

    added simple AI for computer paddle

--- Assets/Scripts/ComputerPaddle.cs
+++ Assets/Scripts/ComputerPaddle.cs
@@ -2,5 +2,33 @@ using UnityEngine;
 
 public class ComputerPaddle : Paddle
 {
+    // in editor, drag the ball to the rigidbody
+    public Rigidbody2D ball;
 
+    // track position of the ball
+    private void FixedUpdate()
+    {
+        // ball moving right, towards computer
+        if (this.ball.velocity.x > 0.0f)
+        {
+            if (this.ball.position.y > this.transform.position.y)
+            {
+                _rigidBody.AddForce(Vector2.up * this.speed);
+            } else if (this.ball.position.y < this.transform.position.y)
+            {
+                _rigidBody.AddForce(Vector2.down * this.speed);
+            }
+        } 
+        else
+        {
+            // greater than 0 means that its above the center line
+            if (this.transform.position.y > 0.0f)
+            {
+                _rigidBody.AddForce(Vector2.down * this.speed);
+            } else
+            {
+                _rigidBody.AddForce(Vector2.up * this.speed);
+            }
+        }
+    }
 }
